efeb377f2a99fe72a2494cb7e7a020c09cd4d75b
Surya Haditama <110581874+Haditamaaa@users.noreply.github.com>
2024-11-22 16:57:53 +0700

    filament

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\CategoryResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                Forms\Components\Textinput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textinput::make('tagline')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('icon')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/WorkshopInstructor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class WorkshopInstructor extends Model
+{
+    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'occupation',
+        'avatar',
+    ];
+
+    public function workshops(): HasMany
+    {
+        return $this->hasMany(Workshop::class);
+    }
+}
